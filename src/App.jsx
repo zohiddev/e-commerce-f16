3ba0323058bd0c1dcab5dfd7812364bcbb2a4186
alibekkarimov717 <alibekkarimovvv@gmail.com>
2023-08-22 17:32:53 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Header from "./layout/Header"
-
+import Brands from "./layout/Main/brands"
 function App() {
 
   return (
     <div className="wrapper">
       <Header />
-
+      <Brands />
 
     </div>
   )
