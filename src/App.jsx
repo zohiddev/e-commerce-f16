28eca47ae8315e1b9c0e9479a933402abf7136c2
2-zona 8-komp <fafadada2000@mail.ru>
2023-08-22 17:39:24 +0500

    olcha

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Header from "./layout/Header"
+import Swiper from "./layout/Main/swiper"
 
 function App() {
 
   return (
     <div className="wrapper">
       <Header />
-
-
+      <Swiper/>
     </div>
   )
 }
