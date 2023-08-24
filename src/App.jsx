0a675f47bc80ad1976ece3bd01f0cb4a0856ea19
2-zona 4-komp <islomotajanov636@gmail.com>
2023-08-24 18:05:34 +0500

    project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./layout/Header"
+import LinkImages from "./layout/Main/LinkImages"
+import NotesCards from "./layout/Main/NotesCards"
 
 function App() {
 
   return (
     <div className="wrapper">
       <Header />
-
-
+      <LinkImages/>
+      <NotesCards/>
     </div>
   )
 }
