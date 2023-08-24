d8711466f69d5c9faefbd6d3e639642de4ac20f4
2-zona 8-komp <botirovdev7@gmail.com>
2023-08-24 18:01:52 +0500

    added prouct

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./layout/Header"
+import AppleProducts from "./layout/Main/AppleProducts"
+import Swiper from "./layout/Main/swiper"
 
 function App() {
 
   return (
     <div className="wrapper">
       <Header />
-
-
+      <Swiper/>
+      <AppleProducts/>
     </div>
   )
 }
---
+++ src/assets/icons/RightArrowIcon.jsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+const RightArrowIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={6}
+    height={11}
+    viewBox="0 0 6 11"
+    fill="none"
+  >
+    <path
+      d="M0.749939 9.98047L5.24994 5.48047L0.749939 0.980469"
+      stroke="#DA002B"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
+export default RightArrowIcon
